ad2897c59fb2ad515fbacbbd9f662a5eec9ac971
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-14 20:46:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-    programmer_hash[:alan_kay][:known_for] = :alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = [:alans_new_info = "GUI"]
 end
 
 def adding_to_dennis
