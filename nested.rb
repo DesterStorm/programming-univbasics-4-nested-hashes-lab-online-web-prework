c00049091a167efd82d99da4d44938fdc6d7225f
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-14 20:30:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,9 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for 
      
-     programmer_hash[:alan_kay][:known_for][
+     programmer_hash[:alan_kay][:known_for][alans_new_info] = "GUI"
      puts programmer_hash
-     
 end
 
 def adding_to_dennis
